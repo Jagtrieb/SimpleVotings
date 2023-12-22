18732fce26db2b4ddb497bfede6f8c7c3a35f24e
Jagtrieb <Boegor3105@gmail.com>
2023-12-22 19:59:12 +0300

    Adding working websitegit add *!

--- Votings/Votings/settings.py
+++ Votings/Votings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Votings'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Votings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['SuperVotings/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
