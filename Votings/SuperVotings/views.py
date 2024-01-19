6585c57c910aeb0cd3f2f60daea258369ad934e7
Jagtrieb <Boegor3105@gmail.com>
2024-01-19 19:40:55 +0300

    Fixing views

--- Votings/SuperVotings/views.py
+++ Votings/SuperVotings/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-from SuperVotings.forms import RadioForm
+
 
 def index(request):
     context = {}
     context['name'] = 'user'
 
-
-
     return render(request, 'index.html', context)
+
 
 def authorization(request):
     return render(request, 'authorization.html')
 
+
 def restore_page(requset):
     return render(requset, 'restore_page.html')
 
+
 def signup(request):
     return render(request, 'signup.html')
+
 
 def voting_page(requset):
     context = {}
