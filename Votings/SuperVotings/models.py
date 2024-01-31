2f7fadc9e112e3e2cf3304f7f23faa73456c41af
Misha <gitenjoyer@gmail.com>
2024-01-31 21:39:44 +0300

    много всего вроде работает

--- Votings/SuperVotings/models.py
+++ Votings/SuperVotings/models.py
@@ -6,8 +6,7 @@ class Vote(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     mode = models.IntegerField(default=0)
-
-class Option(models.Model):
+class Participant(models.Model):
     name = models.CharField(max_length=64)
+    vote_id = models.IntegerField(default=0)
     count = models.IntegerField(default=0)
-    vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
--- Votings/SuperVotings/views.py
+++ Votings/SuperVotings/views.py
@@ -5,12 +5,13 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect
 from SuperVotings.forms import RadioForm, AddSnippetForm
 import random
-from SuperVotings.models import Vote
+from SuperVotings.models import Vote, Participant
 def index(request):
     context = {}
     context['votes'] = Vote.objects.all()
     spisok=context['votes']
-    context['id'] = [spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)]]
+    if len(spisok) != 0:
+        context['id'] = [spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)], spisok[random.randint(0, len(context['votes'])-1)]]
     return render(request, 'index.html', context)
 
 def authorization(request):
@@ -52,17 +53,29 @@ def votes_view(request: WSGIRequest, id: int):
         return render(request, "404.html")
     context["pagename"] = vote[0].title
     context["vote"] = vote[0]
+    context['participants'] = Participant.objects.filter(vote_id=id)
+    if request.method == 'POST':
+        participant_id = request.POST.get("radio")
+        participant = Participant.objects.filter(id=participant_id)
+        count=participant[0].count
+        participant.update(count=(count+1))
+        print(participant[0].count)
     return render(request, 'pages/vote.html', context)
 
 def votes_create1(request):
     context={}
     if request.method == 'POST':
-        if request.POST.get("title") != "" or request.POST.get("description") != "":
+        if request.POST.get("title") != "" and request.POST.get("description") != "" and request.POST.get("1") != "" and request.POST.get("2") != "" and request.POST.get("3") != "":
             title = request.POST.get("title", "Голосование")
             description = request.POST.get("description", "Голосуйте")
             vote = Vote(title=title, description=description, mode=1)
             vote.save()
             id = vote.id
+            for i in range(1, 4):
+                name = request.POST.get(str(i))
+                participant = Participant(name=name, vote_id=id)
+                participant.save()
+            context['participants'] = Participant.objects.filter(vote_id=id)
             context['votes'] = Vote.objects.all()
             messages.add_message(request, messages.SUCCESS, "Сниппет успешно добавлен")
             return redirect('vote_view', id=id)
